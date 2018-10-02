049c495a0f090ed66ca99af9e382e66ba793520d
Aniruddha Bhamare <ani.fullstack@gmail.com>
2018-10-02 02:06:26 -0700

    added create message and new message custom event

--- server/server.js
+++ server/server.js
@@ -1,14 +1,49 @@
 const path = require('path');
 const express = require('express');
-
-var app = express();
-var port = process.env.PORT || 3000;
+const socketIO = require('socket.io');
+const http = require('http');
 
 var publicPath = path.join(__dirname + '/../public');
-console.log(publicPath);
+var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/../public'));
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
-app.listen(port,()=>{
+app.use(express.static(publicPath));
+
+io.on('connection',(socket)=>{
+console.log('New User is Connected!');
+
+//Example Emit newEmail from Cl
+// socket.emit('newEmail',{
+//     from:"sender@email.com",
+//     text:"hello user",
+//     createdAt:124
+// });
+
+//Example Listner form Client mesgs
+//socket.on('createEmail',function(newEmail){
+//console.log('createEmail',newEmail);
+//});
+
+//create newMessage Emit
+socket.emit('newMessage',{
+    to:"client@client.com",
+    text:"hello client"
+});
+
+
+//createMessage Listner
+socket.on('createMessage',function(msg){
+    console.log("incoming createdMessage from client",msg);
+});
+
+ socket.on('disconnect',()=>{
+     console.log('User was disconnected');
+ });   
+});
+
+server.listen(port,()=>{
     console.log(`server is running on port ${port}`);
 });
