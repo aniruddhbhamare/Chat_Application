61c08a020584d555b09c5240f103ca1eac8bf7cc
Aniruddha Bhamare <ani.fullstack@gmail.com>
2018-10-02 02:32:45 -0700

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,34 +2,22 @@ var socket = io();
 
 socket.on('connect',function() {
     console.log('Connected to Server');
-
-    //Example client Emit 
-
-    // socket.emit('createEmail',{
-    //     to:'server',
-    //     text:'testing',
-    //     createdAt:234
-    // });
-
     //new Message Listner
-    socket.on('newMessage',function(nmsg){
-        console.log("messageFrom Client",nmsg);
+    socket.on('newMessage',function(msg){
+        console.log("newMessage",msg);
     });
 
     //emit msg from client to server
-    socket.emit('createMessage',{
-        from:"client@client.com",
-        text:"hello all clients",
-        createdAt:123
-    });
+    // socket.emit('createMessage',{
+    //     from:"client@client.com",
+    //     text:"hello all clients",
+    //     createdAt:123
+    // });
 });
 
 socket.on('disconnect',function() {
     console.log('disconnected from server');
 });
 
-    //Example Listner for new email from server
-    // socket.on('newEmail',function(email){
-    // console.log('new Email',email);
-    //});
+
 
--- server/server.js
+++ server/server.js
@@ -14,35 +14,28 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
 console.log('New User is Connected!');
-
-//Example Emit newEmail from Cl
-// socket.emit('newEmail',{
-//     from:"sender@email.com",
-//     text:"hello user",
-//     createdAt:124
-// });
-
-//Example Listner form Client mesgs
-//socket.on('createEmail',function(newEmail){
-//console.log('createEmail',newEmail);
-//});
-
 //create newMessage Emit
-socket.emit('newMessage',{
-    to:"client@client.com",
-    text:"hello client"
-});
-
-
+// socket.emit('newMessage',{
+//     to:"client@client.com",
+//     text:"hello client"
+// });
 //createMessage Listner
 socket.on('createMessage',function(msg){
     console.log("incoming createdMessage from client",msg);
+    io.emit('newMessage',{
+        from:msg.from,
+        createdAt:new Date().getTime(),
+        text:msg.text
+    });
 });
 
  socket.on('disconnect',()=>{
      console.log('User was disconnected');
- });   
+ });
+ 
+
 });
+
 
 server.listen(port,()=>{
     console.log(`server is running on port ${port}`);
